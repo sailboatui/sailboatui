fb7bd7d73f0dd275b074fed2125f3079984d0cc4
lvwzhen <lvwzhen@gmail.com>
2023-01-03 10:33:17 +0800

    Fix

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,4 @@
 module.exports = {
   tabWidth: 2,
   printWidth: 120,
-  overrides: [
-    {
-      files: ["*.html"],
-      options: {
-        parser: "go-template",
-      },
-    },
-  ],
-  plugins: [require("prettier-plugin-go-template"), require("prettier-plugin-tailwindcss")],
 };
